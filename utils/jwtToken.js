875513e48ec595b740c9fb5d3546fb76f6e5ea0b
Rahul Bansal <149091896+Bansalrahul2004@users.noreply.github.com>
2025-03-11 00:03:57 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,11 +1,11 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
+  const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRES) || 10; // Convert "10d" to number
+
   res
     .status(statusCode)
     .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000), // Convert days to milliseconds
       httpOnly: true,
       sameSite: "None",
       secure: true,
@@ -17,4 +17,3 @@ export const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
